31e18321d955a74e10b73be5c56dc3b02601950f
MinZhu123 <84722601+MinZhu123@users.noreply.github.com>
2023-05-10 14:58:18 -0400

    Create deeponet.py

---
+++ src/burgers/DeepONet_pretrain/deeponet.py
@@ -0,0 +1,52 @@
+import os
+os.environ['DDE_BACKEND'] = 'tensorflow.compat.v1'
+import numpy as np
+import deepxde as dde
+from deepxde.backend import tf
+
+
+def gelu(x):
+    return 0.5 * x * (1 + tf.math.tanh(tf.math.sqrt(2 / np.pi) * (x + 0.044715 * x ** 3)))
+
+
+def main():
+    ls_train = 1.0
+    ls_test = 1.0
+    lr = 0.001
+    iterations = 500000
+    m_train = 101
+    m_test = 101
+    train_data = np.load(f"../../../data/burgers/burgers_train_ls_{ls_train}_{m_train}_{m_train}.npz")
+    test_data = np.load(f"../../../data/burgers/burgers_test_ls_{ls_test}_{m_test}_{m_test}.npz")
+    X_train = (np.repeat(train_data["X_train0"], m_train*m_train, axis=0), np.tile(train_data["X_train1"], (1000, 1)))
+    y_train = train_data["y_train"].reshape(-1, 1)
+    X_test = (np.repeat(test_data["X_test0"], m_test*m_test, axis=0), np.tile(test_data["X_test1"], (100, 1)))
+    y_test = test_data["y_test"].reshape(-1, 1)
+    data = dde.data.Triple(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test)
+
+    net = dde.maps.DeepONet(
+        [m_train, 100, 100, 100],
+        [2, 100, 100, 100],
+        {"branch": "relu", "trunk": gelu},
+        "Glorot normal",
+        regularization=["l2", 1e-5]
+    )
+
+    def periodic(x):
+        return tf.concat([tf.math.cos(x[:, 0:1] * 2 * np.pi), tf.math.sin(x[:, 0:1] * 2 * np.pi),
+                          tf.math.cos(2 * x[:, 0:1] * 2 * np.pi), tf.math.sin(2 * x[:, 0:1] * 2 * np.pi),
+                          tf.math.cos(3 * x[:, 0:1] * 2 * np.pi), tf.math.sin(3 * x[:, 0:1] * 2 * np.pi),
+                          tf.math.cos(4 * x[:, 0:1] * 2 * np.pi), tf.math.sin(4 * x[:, 0:1] * 2 * np.pi), x[:, 1:2]], 1)
+
+    net.apply_feature_transform(periodic)
+
+    model = dde.Model(data, net)
+    model.compile("adam", lr=lr, metrics=["l2 relative error"], decay=("inverse time", iterations // 5, 0.5))
+
+    checker = dde.callbacks.ModelCheckpoint("model/model.ckpt", save_better_only=False, period=1000)
+    losshistory, train_state = model.train(iterations=iterations, callbacks=[checker], batch_size=30000)
+    dde.saveplot(losshistory, train_state, issave=True, isplot=False)
+
+
+if __name__ == "__main__":
+    main()
